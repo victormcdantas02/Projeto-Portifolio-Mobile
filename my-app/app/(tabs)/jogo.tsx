903f5edfbc989a22f45a6917cef238775d99148d
Victor <victormcdantas@gmail.com>
2025-10-28 20:48:07 -0300

    Update tab jogo iniciada

---
+++ my-app/app/(tabs)/jogo.tsx
@@ -0,0 +1,5 @@
+import { StyleSheet, ScrollView, View, Text, TouchableOpacity, Linking } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function JogoScreen() {
+  }
